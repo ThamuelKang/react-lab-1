b70c0987a8d6d4c364c76f308f77c5c8ebd749c6
Samuel Kang <samuel.june.kang@gmail.com>
2025-07-23 00:23:12 -0700

    added comments

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import './App.css'
 import { format } from 'date-fns';
 
 function App() {
+  //grabs a new Date
   const currentTime = new Date();
+
+  //date-fns format: set hour, mins, and AM/PM
   const formattedTime = format(currentTime, 'hh:mm a')
+
+  //date-fns format: day, year, month, date
   const formattedDate = format(currentTime, 'EEEE, MMMM d, yyyy')
 
   return (
     <header className="main">
       <p>Currently</p>
 
+      {/* react components */}
       <h1>{formattedTime}</h1>
       <h2>{formattedDate}</h2>
     </header>
